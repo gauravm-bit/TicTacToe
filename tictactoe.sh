1626ea0cbdf26edbb3e5d4ce47f5ab22a5e1e48b
gauravm-bit <gauravwrk01@gmail.com>
2020-03-11 12:10:35 +0530

    ADDED:resetting the board function has been added

--- tictactoe.sh
+++ tictactoe.sh
@@ -2,4 +2,21 @@
 
 echo "Welcome to tictactoe problem"
 
+#Constants
+ROWNUMBER=3
+COLNUMBER=3
+
+declare -A board
+
+function reset() {
+	for (( row=0; row <= ROWNUMBER; row++ ))
+	do
+		for (( col=0; col <= COLNUMBER; col++ ))
+		do
+			board[$row,$col]=""
+		done
+	done
+}
+reset
+
 

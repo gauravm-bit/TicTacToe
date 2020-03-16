64b3c91975bb9e963a5233c3dc455e91cec43f8f
gauravm-bit <gauravwrk01@gmail.com>
2020-03-16 16:04:27 +0530

    REFACTORED: win function splitted into smaller functions

--- tictactoe.sh
+++ tictactoe.sh
@@ -4,213 +4,165 @@ echo "Welcome to tictactoe problem"
 
 #Constants
 ROWNUMBER=3
-COLNUMBER=3
+COLUMNNUMBER=3
 TOTALCOUNT=9
 
 #variables
-assign=$((RANDOM%2))
-toss=$((RANDOM%2))
+randomCheck=$((RANDOM%2))
 playcount=0
 
 declare -A board
 
 function reset() {
-	for (( row=0; row < ROWNUMBER; row++ ))
-	do
-		for (( col=0; col < COLNUMBER; col++ ))
-		do
-			board[$row,$col]=" "
-		done
-	done
+   for (( row=0; row < $ROWNUMBER; row++ ))
+   do
+      for (( col=0; col < $COLUMNNUMBER; col++ ))
+      do
+         board[$row,$col]=" "
+      done
+   done
 }
 
 function assignLetter() {
-	if (( $assign == 0 ))
-	then
-		player="X"
-		computer="O"
-	else
-		player="O"
-		computer="X"
-	fi
-	echo "Player has been assigned $player"
+   if (( $randomCheck == 0 ))
+   then
+      player="X"
+      computer="O"
+   else
+      player="O"
+      computer="X"
+   fi
+   echo "Player has been assigned $player"
    echo "Computer has been assigned $computer"
 
 }
 
 function boardShow(){
-	for (( row=0; row < ROWNUMBER; row++ ))
+   for (( row=0; row < $ROWNUMBER; row++ ))
    do
-      for (( col=0; col < COLNUMBER; col++ ))
+      for (( col=0; col < $COLUMNNUMBER; col++ ))
       do
-			if (( col < 2 ))
-			then
-         	printf "${board[$row,$col]} | "
-			else
-				printf "${board[$row,$col]}"
-			fi
+         if (( col < 2 ))
+         then
+            printf "${board[$row,$col]} | "
+         else
+            printf "${board[$row,$col]}"
+         fi
       done
-	if (( row < 2 ))
-	then
-		printf "\n----------\n"
-	fi
+   if (( row < 2 ))
+   then
+      printf "\n----------\n"
+   fi
    done
-		printf "\n"
+      printf "\n"
 }
 
-function checkWin(){
+function checkVertical(){
+	playerinput=$1
+	for ((row=0; row < $ROWNUMBER; row++))
+	do
+		for ((column=0; column < $COLUMNNUMBER; column++))
+		do
+	  		 if [[ ${board[$row,$column]}${board[$(($row+1)),$column]}${board[$(($row+2)),$column]} == $playerinput$playerinput$playerinput ]]
+			 then
+				flag=true
+				echo "$flag"
+				return
+			fi
+		done
+	done
+}
+
+function checkHorizontal(){
+	playerinput=$1
+	for ((row=0; row<3; row++))
+	do
+		for ((column=0; column<3; column++))
+		do
+			if [[ ${board[$row,$column]}${board[$row,$(($column+1))]}${board[$row,$(($column+2))]} == $playerinput$playerinput$playerinput ]]
+			then
+				flag=true
+				echo "$flag"
+				return
+			fi
+		done
+	done
+}
+
+function checkDiagonal(){
 	playerinput=$1
 	row=0
 	column=0
-	flag=false
-
-	while [ $column -lt $COLNUMBER ]
-	do
-		if [[ ${board[$row,$column]}${board[$(($row+1)),$column]}${board[$(($row+2)),$column]} == $playerinput$playerinput$playerinput ]]
-		then
-			flag=true
-			echo "$flag"
-			return
-		fi
-		((column++))
-	done
-
-	row=0
-	column=0
-
-	while [ $row -lt $ROWNUMBER ]
-	do
-		if [[ ${board[$row,$column]}${board[$row,$(($column+1))]}${board[$row,$(($column+2))]} == $playerinput$playerinput$playerinput ]]
-		then
-			flag=true
-			echo "$flag"
-			return
-		fi
-		((row++))
-	done
-
-	row=0
-	column=0
-
-	if [[ ${board[$row,$column]}${board[$(($row+1)),$(($column+1))]}${board[$(($row+2)),$(($column+2))]} == $playerinput$playerinput$playerinput ]]
+	diagonal1=${board[$row,$column]}${board[$(($row+1)),$(($column+1))]}${board[$(($row+2)),$(($column+2))]}
+	diagonal2=${board[$row,$(($column+2))]}${board[$(($row+1)),$(($column+1))]}${board[$(($row+2)),$column]}
+	if [[ $diagonal1 == $playerinput$playerinput$playerinput || $diagonal2 == $playerinput$playerinput$playerinput ]]
 	then
 		flag=true
 		echo "$flag"
 		return
 	fi
-
-	row=0
-	column=$(($column+2))
-
-	if [[ ${board[$row,$column]}${board[$(($row+1)),$(($column-1))]}${board[$(($row+2)),$(($column-2))]} == $playerinput$playerinput$playerinput ]]
-	then
-		flag=true
-		echo "$flag"
-		return
-	fi
-
-	echo "$flag"
 }
-function playerTurn(){
-		count=1
-		if [[	$playCount == $TOTALCOUNT ]]
-		then
-			echo "Match tie"
-			exit
-		fi
-		read -p "Player turn, Enter player position :" position
-		for (( i=0; i<$ROWNUMBER; i++ ))
-		do
-			for (( j=0; j<$COLNUMBER; j++))
-			do
-				if [[ $count == $position ]]
-				then
-					if [[ ${board[$i,$j]} == " " ]]
-					then
-						board[$i,$j]=$player
-					else
-						printf "\nInvalid move,Enter again\n"
-						playerTurn
-					fi
-				fi
-			((count++))
-			done
-		done
 
-	((playCount++))
-	 boardShow
-	if	[[ $(checkWin $player) == true ]]
+
+
+function checkWin(){
+   playerinput=$1
+   flag=false
+	checkVertical $playerinput
+	checkHorizontal $playerinput
+	checkDiagonal $playerinput
+
+	}
+
+function tieCheck(){
+	playCount=$1
+	if [[ $playCount == $TOTALCOUNT ]]
 	then
-		printf "player won"
+		echo	"Match tie"
 		exit
 	fi
-	computerTurn
 }
 
-function winningLogic(){
-    input=$1
-   for (( i=0; i<$ROWNUMBER; i++ ))
-   do
-      if [[ ${board[$i,0]}${board[$i,1]} == $input$input ]]
-      then
-         if [[ ${board[$i,2]} == " " ]]
-         then
-            board[$i,2]=$input
-            flag="true"
-         fi
-      elif [[ ${board[$i,1]}${board[$i,2]} == $input$input ]]
-		then
-			if [[ ${board[$i,0]} == " " ]]
-			then
-				board[$i,0]=$input
-				flag="true"
-			fi
-		elif [[ ${board[$i,2]}${board[$i,0]} == $input$input ]]
-		then
-			if [[ ${board[$i,0]} == " " ]]
-			then
-				board[$i,0]=$input
-				flag="true"
-			fi
-		fi
-	done
-	for (( i=0; i<$COLNUMBER; i++ ))
-	do
-		if [[ ${board[0,$i]}${board[1,$i]} == $input$input ]]
-		then
-			if [[ ${board[2,$i]} == " " ]]
-			then
-				board[2,$i]=$input
-				flag="true"
-			fi
-		elif [[ ${board[1,$i]}${board[2,$i]} == $input$input ]]
-		then
-			if [[ ${board[0,$i]} == " " ]]
-			then
-				board[0,$i]=$input
-				flag="true"
-			fi
-		elif [[ ${board[2,$i]}${board[0,$i]} == $input$input ]]
-		then
-			if [[ ${board[1,$i]} == " " ]]
-			then
-				board[1,$i]=$input
-				flag="true"
-			fi
-	done
-function computerTurn(){
-		count=0
-      if [[ $playCount == $TOTALCOUNT ]]
-      then
-         echo "Match tie"
-      exit
-      fi
-      printf "\nComputer turn\n"
-		position=$(( RANDOM%9 + 1 ))
-
-		for (( i=0; i<$ROWNUMBER; i++ ))
+function playerTurn(){
+      count=1
+      tieCheck $playCount
+      read -p "Player turn, Enter player position :" position
+      for (( i=0; i<$ROWNUMBER; i++ ))
       do
-         for (( j=0; j<$COLNUMBER; j++ ))
+         for (( j=0; j<$COLUMNNUMBER; j++))
+         do
+            if [[ $count == $position ]]
+            then
+               if [[ ${board[$i,$j]} == " " ]]
+               then
+                  board[$i,$j]=$player
+               else
+                  printf "\nInvalid move,Enter again\n"
+                  playerTurn
+               fi
+            fi
+         ((count++))
+         done
+      done
+	((playCount++))
+    boardShow
+   if [[ $(checkWin $player) == true ]]
+   then
+      printf "player won"
+      exit
+   fi
+   computerTurn
+}
+
+function computerTurn(){
+      count=1
+		tieCheck $playCount
+      printf "\nComputer turn\n"
+      position=$(( RANDOM%9 + 1 ))
+
+      for (( i=0; i<$ROWNUMBER; i++ ))
+      do
+         for (( j=0; j<$COLUMNNUMBER; j++ ))
          do
             if [[ $count == $position ]]
             then
@@ -226,17 +178,17 @@ function computerTurn(){
          done
       done
    ((playCount++))
-	boardShow
+   boardShow
    if [[ $(checkWin $computer) == true ]]
    then
       printf "computer won"
-		exit
+      exit
    fi
    playerTurn
 }
 
 function Toss(){
-   if (( $toss == 0 ))
+   if (( $randomCheck == 0 ))
    then
       echo "Player  has won the toss"
       playerTurn
